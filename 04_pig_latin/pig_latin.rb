eb5f2af78bd22b8416d93765b6767b300d7e82cc
nate dawg <nglass1988@gmail.com>
2013-09-16 16:23:53 -0700

    more piglatin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,6 +1,5 @@
-def translate(word)
-
-
+def wordcheck(word)
+	
   if "aeiou".include?(word[0])
   	word << "ay"
   else 
@@ -9,15 +8,46 @@ def translate(word)
   		word[0] = ''
  			word << tmp << "ay"
 	  else
-	  	tmp0 = word[0] << word[1]
-	  	word[0] = ""
-	  	word[0] = ""
-
-	  	word << tmp0 << "ay"
-
+	  	if "aeiou".include?(word[2])
+	  		tmp0 = word[0] << word[1]
+	  		word[0] = ""
+	  		word[0] = ""
+	  		word << tmp0 << "ay"
+	  	else
+	  		tmp1 = word[0] << word[1] << word[2]
+	  		3.times { word[0] = '' }
+	  		word << tmp1 << "ay"
+	  	end
+	  	
 	  end
-  	
   end
 end
-print translate("chesdfssakd")
+
+def firstTwo(word)
+	tmp0 = word[0] << word[1]
+	print word
+
+	word[0] = ''
+		print word
+
+	word[0] = ''
+		print word
+
+	word << tmp0 << "ay"
+end
+
+def translate(sentence)
+
+	if sentence.gsub(' ', '') != sentence
+		wordarr = sentence.split(' ')
+		wordarr.map { |word| wordcheck(word) }.join(" ")
+	else
+		if sentence[0] == 'q' && sentence[1] == 'u'
+ 			firstTwo(sentence)
+ 		else
+			wordcheck(sentence)
+		end
+	end
+end
+print translate("quiet")
 
